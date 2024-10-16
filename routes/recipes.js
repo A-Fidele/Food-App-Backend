4e506a6ef60fefce925f067c9451474fe9a6323f
Fidele <jackie.fidele@laposte.net>
2024-10-16 11:09:21 +0200

    Test recipes get method

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,22 @@
 var express = require("express");
+const Recipe = require("../models/recipes");
 var router = express.Router();
 require("../models/connection");
 
 /* GET home page. */
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  Recipe.find()
+    .then((data) => {
+      if (data) {
+        res.json({ result: true, recipes: data });
+      } else {
+        res.json({ result: false, message: "No recipes found" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error in fectching recipes", error);
+      res.status(500).json({ result: false, error: "Erreur serveur" });
+    });
+});
 
 module.exports = router;
