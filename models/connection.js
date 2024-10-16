d1862e775312dc695affd9da5dab6316f1c3761e
Fidele <jackie.fidele@laposte.net>
2024-10-16 10:56:24 +0200

    Fix errors in connection

--- models/connection.js
+++ models/connection.js
@@ -4,9 +4,7 @@ const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     connectTimeoutMS: 5000,
   })
   .then(() => console.log("Database Food-App connected!"))
-  .then((error) => console.error(error));
+  .catch((error) => console.error(error));
